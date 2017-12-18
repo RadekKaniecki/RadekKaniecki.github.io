2f4d11b945748fac145785f06a757a6cdd442175
RadekKaniecki <radoslawkaniecki@gmail.com>
2017-12-18 21:40:13 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://young-savannah-18112.herokuapp.com/v1';
+  const apiRoot = 'https://young-savannah-18112.herokuapp.com/v1/';
   const trelloApiRoot = 'https://young-savannah-18112.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
